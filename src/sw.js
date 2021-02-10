a1649f20df4b402c06f3a3a199100e539c97192d
Stuart Rutherford <squirrel96@live.co.uk>
2021-02-10 23:24:36 +1100

    Add setupRouting to sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,15 +1,7 @@
-/**
-import { getFiles, setupPrecaching } from 'preact-cli/sw/';
-
-const urlsToCache = getFiles();
-urlsToCache.push({ url: '/data/learnsets.json', revision: '2021-01-29' });
-
-setupPrecaching(urlsToCache);
-*/
-
-import { getFiles, setupPrecaching } from 'preact-cli/sw/';
+import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
 
 const urlsToCache = getFiles();
 urlsToCache.push({ url: '/data/learnsets.json', revision: '2021-02-06' });
 
+setupRouting();
 setupPrecaching(urlsToCache);
